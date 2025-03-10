6e92280ecefec7f53ffc4ef0da248b4255d1c9ac
LADE SAI TEJA <162271890+ladesai123@users.noreply.github.com>
2025-03-10 22:15:17 +0530

    Create 10.03.2025MaxProductArray.cpp

---
+++ 10.03.2025MaxProductArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+    
+        int prefix=1;
+        int suffix=1;
+        int ans=INT_MIN;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(prefix==0){
+                prefix=1;
+            }
+            if(suffix==0){
+                suffix=1;
+            }
+            prefix*=arr[i];
+            suffix*=arr[n-i-1];
+            ans=max(ans,max(prefix,suffix));
+        }
+        return ans;
+    }
+};
